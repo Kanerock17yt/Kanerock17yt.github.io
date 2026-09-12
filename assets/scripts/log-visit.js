23cfb24710014ecbda464bc0e59a10d5b1b965a0
Kane_rock17yt <kanerock17@gmail.com>
2026-09-12 16:14:08 -0400

    why do you care what i change

---
+++ assets/scripts/log-visit.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDA414TIYWu_c8OKWj1GvgrGZbYzGPzv6U",
+    authDomain: "counter-508020.firebaseapp.com",
+    databaseURL: "https://youtube-counter-508020-default-rtdb.firebaseio.com",
+    projectId: "youtube-counter-508020",
+    storageBucket: "youtube-counter-508020.firebasestorage.app",
+    messagingSenderId: "600998318105",
+    appId: "1:600998318105:web:9835946bd363b3b57cda88"
+};
+
+const SECRET_KEY = '#EÅÏ"àë.×θÞyσJÒÙæØyXâTχË2HPoè3öÀâ[ÓNEPsοÄυîHjh(CÒÀPÐ}>M*{ÑfGoμCêKÈ"VGÄuÅç~1G?ζξLZ%MοÑÞK[òÂCYÒpöl8:';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+signInAnonymously(auth)
+    .then(() => {
+        const visitorData = JSON.stringify({
+            page: window.location.pathname,
+            time: new Date().toLocaleTimeString()
+        });
+
+        const encryptedPayload = CryptoJS.AES.encrypt(visitorData, SECRET_KEY).toString();
+
+        push(ref(db, 'active-visitors'), {
+            payload: encryptedPayload
+        });
+    })
+    .catch((error) => {
+        console.error("Visitor tracking initialization failed:", error);
+    });
